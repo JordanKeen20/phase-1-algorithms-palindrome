27f5abd29d9b4053079094b1428b90df668dad9c
jordanlynne9@gmail.com <jordanlynne9@gmail.com>
2022-08-03 00:40:20 -0700

    commit tree fc04cb7198313b4ffb59e9f6913c76969f310779

--- index.js
+++ index.js
@@ -1,25 +1,51 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  // My algorithm 
+  if(word.split('').reverse().join('') === word){
+      return true}
+    else {
+        return false}
 }
+isPalindrome('rob')
+
 
 /* 
-  Add your pseudocode here
+if you put in: 'bob'
+what will return : true
+
+if you put in : 'robby'
+what will return : false
 */
 
 /*
-  Add written explanation of your solution here
+Written Explanation:
+The function will take one argument, that is a string. 
+The string will then be split appart, reversed, and then joined again.
+Thats when it will be determind if it is true or false. 
+If the word is the same as what was passed through the funcion
+it will come back true. If it is different it will come back false.
+
 */
 
 // You can run `node index.js` to view these console logs
-if (require.main === module) {
-  // add your own custom tests in here
-  console.log("Expecting: true");
-  console.log("=>", isPalindrome("racecar"));
+ if (require.main === module) {
+// my custom tests
+  console.log('expecting: true')
+  console.log('=>', isPalindrome('redivider'))
 
-  console.log("");
+  console.log(' ')
 
-  console.log("Expecting: false");
-  console.log("=>", isPalindrome("robot"));
+  console.log('expecting: false')
+  console.log('=>', isPalindrome('Allibaba'))
+
+  console.log(' ')
+
+  // console.log("Expecting: true");
+  // console.log("=>", isPalindrome("racecar"));
+
+  // console.log("");
+
+  // console.log("Expecting: false");
+  // console.log("=>", isPalindrome("robot"));
 }
 
 module.exports = isPalindrome;
